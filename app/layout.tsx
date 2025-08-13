02cffe524d2e709d33b29a00829373f5137badb2
doctorzik <ezekieljeremiah92@gmail.com>
2025-08-13 23:01:31 +0100

    prisma generate script updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,8 @@ import './globals.css';
 import { NavBar } from '@/components/molecules/NavBar';
 import { SessionProvider } from 'next-auth/react';
 import type { ReactNode } from 'react';
+import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/next"
 
 export default function RootLayout({
   children,
@@ -13,7 +15,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <SpeedInsights />
+      <Analytics />
       <head />
+
       <body>
         <SessionProvider >
           <NavBar />
